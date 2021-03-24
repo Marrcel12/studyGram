21f58867ebbb9dd9978b74ebc04e730992403677
Marcel <marcel.kaminski@ucdconnect.ie>
2021-03-24 10:44:50 +0100

    product add

--- studyGram/marketplace/models.py
+++ studyGram/marketplace/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class product(models.Model):
+    title = models.CharField(max_length=80)
+    thumbnail = models.ImageField(upload_to='cars')
+    description = models.CharField(max_length=500)
+    rate = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    author = models.CharField(max_length=80)
+    product_id = models.AutoField(primary_key=True)
