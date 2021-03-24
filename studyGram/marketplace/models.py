4c9b3dd12901ebf5607dd9936a6c8ecee60e5194
Marcel <marcel.kaminski@ucdconnect.ie>
2021-03-24 10:46:09 +0100

    new_media

--- studyGram/marketplace/models.py
+++ studyGram/marketplace/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class product(models.Model):
     title = models.CharField(max_length=80)
-    thumbnail = models.ImageField(upload_to='cars')
+    thumbnail = models.ImageField(upload_to='thumbnails_products')
     description = models.CharField(max_length=500)
     rate = models.CharField(max_length=30)
     price = models.DecimalField(max_digits=6, decimal_places=2)
