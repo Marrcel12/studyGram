3321c0b7139fafa1d00db4574a6d6f5e9de2e410
Marcel <marcel.kaminski@ucdconnect.ie>
2021-03-24 11:11:39 +0100

    First version of models structure

--- studyGram/marketplace/models.py
+++ studyGram/marketplace/models.py
@@ -1,11 +1,38 @@
 from django.db import models
+from profiles.models import profile
 
 
 class product(models.Model):
-    title = models.CharField(max_length=80)
-    thumbnail = models.ImageField(upload_to='thumbnails_products')
-    description = models.CharField(max_length=500)
-    rate = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    author = models.CharField(max_length=80)
     product_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=80, blank=False)
+    thumbnail = models.ImageField(upload_to='thumbnails_products')
+    description = RichTextField(
+        max_length=2500, default="", blank=True, verbose_name="Opis produktu", blank=True)
+    rate = models.CharField(max_length=30, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
+    author_id = models.ForeignKey(profile)
+    monthly_rating = models.DecimalField(
+        max_digits=4, decimal_places=0, blank=True)
+    tags = models.CharField(max_length=500, blank=False)
+
+
+class photo(models.Model):
+    id_photo = models.AutoField(primary_key=True)
+    photo_file = models.ImageField(upload_to='photo_products')
+
+
+class product_photo(models.Model):
+    id_product_photo = models.AutoField(primary_key=True)
+    id_product = models.ForeignKey(product)
+    id_photo = models.ForeignKey(photo)
+
+
+class category(models.Model):
+    id_category = models.AutoField(primary_key=True)
+    name = = models.CharField(max_length=30, blank=False)
+
+
+class category_product(models.Model):
+    id_category_product = models.AutoField(primary_key=True)
+    id_product = models.ForeignKey(product)
+    id_category = models.ForeignKey(category)
--- studyGram/profiles/models.py
+++ studyGram/profiles/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class profile(models.Model):
+    profile_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=80, blank=False)
+    thumbnail = models.ImageField(upload_to='thumbnails_users')
+    description = RichTextField(
+        max_length=2500, default="", blank=True, verbose_name="Opis usera")
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
+    author = models.CharField(max_length=80, blank=False)
+    # TODO: debug if it is social link
+    facebook_social = models.CharField(max_length=180, blank=True)
+    instagram_social = models.CharField(max_length=180, blank=True)
+
+
+class photo(models.Model):
+    id_photo = models.AutoField(primary_key=True)
+    photo_file = models.ImageField(upload_to='photo_users')
+
+
+class product_photo(models.Model):
+    id_product_photo = models.AutoField(primary_key=True)
+    profile_id = models.ForeignKey(product)
+    id_photo = models.ForeignKey(photo)
